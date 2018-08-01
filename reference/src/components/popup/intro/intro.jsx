44b6b2dcdea69ea1331412376c5e9fb7f2fc51a0
Sergei Demin <s.demin@l412.local>
2018-08-01 13:56:27 +0200

    ADD exp barrier style

--- reference/src/components/popup/intro/intro.jsx
+++ reference/src/components/popup/intro/intro.jsx
@@ -39,14 +39,14 @@ export default class Intro extends Component {
 							We use your data to help bring you personalised content, relevant ads, social media features, and to better understand how you use our website. To do this, we sometimes share this data with social media, advertising, and analytics partners, who may in turn combine it with other data you've given them. Visit our Privacy Policy for more information on our data collection practices.
 						</LocalLabel>
 					</div>
-					<div class={style.acceptBtnWrap}>
-						<Button
-							class={style.acceptAll}
-							onClick={onAcceptAll}
-						>
-							<LocalLabel localizeKey='acceptAll'>Accept</LocalLabel>
-						</Button>
-					</div>
+				</div>
+				<div class={style.acceptBtnWrap}>
+					<Button
+						class={style.acceptAll}
+						onClick={onAcceptAll}
+					>
+						<LocalLabel localizeKey='acceptAll'>Accept</LocalLabel>
+					</Button>
 				</div>
 				<div class={style.option}>
 					<span
