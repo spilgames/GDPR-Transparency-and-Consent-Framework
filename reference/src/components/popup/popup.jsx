ca946ee6143ad944a14fd11e6cf58c0df697b1ae
rodrigodaoud <rodaoud93@gmail.com>
2018-10-31 15:14:36 +0000

    FIX variable from js

--- reference/src/components/popup/popup.jsx
+++ reference/src/components/popup/popup.jsx
@@ -3,6 +3,7 @@ import style from './popup.less';
 import Intro from './intro/intro';
 import Details from './details/details';
 import Panel from '../panel/panel';
+import config from '../../lib/config';
 
 const SECTION_INTRO = 0;
 const SECTION_DETAILS = 1;
@@ -42,6 +43,7 @@ export default class Popup extends Component {
 		const { store } = props;
 		const { selectedPanelIndex } = state;
 		const { isConsentToolShowing } = store;
+		const { logoUrl } = config;
 
 		return (
 			<div
@@ -54,7 +56,7 @@ export default class Popup extends Component {
 				/>
 				<div class={style.content}>
 					<div class={style.logoContainer}>
-						<img class={style.logoImage} src={cmpConfig.logoUrl}/>
+						<img class={style.logoImage} src={logoUrl}/>
 					</div>
 					<Panel selectedIndex={selectedPanelIndex}>
 						<Intro
